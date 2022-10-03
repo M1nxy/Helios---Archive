7428f8139e7001725daa393b3936ea91b10c5f02
Alucard <minxter.yt@gmail.com>
2022-10-03 10:58:07 +0100

    Game command
    A way to check steam and epic games offers

--- src/Commands/Utility/games.ts
+++ src/Commands/Utility/games.ts
@@ -1,78 +1,75 @@
-import { CommandInteraction, Embed, EmbedBuilder, Message, SlashCommandBuilder } from "discord.js";
+import {
+	ButtonStyle,
+	CommandInteraction,
+	Embed,
+	EmbedBuilder,
+	Message,
+	SlashCommandBuilder,
+	TextBasedChannel
+} from "discord.js";
 import { CustomClient, Command } from "../../classes";
 import { getGames } from "epic-free-games";
 import { getColorFromURL } from 'color-thief-node';
 import { getFreePromotions } from 'steam-free-promotions';
-import steam from 'steam-web';
+import SteamApi from 'steam-api';
+import { ChannelPagination, NextPageButton, PreviousPageButton } from "djs-button-pages";
 
-async function gameEmbed(args: string[]) {
+const buttons =[
+	new PreviousPageButton({custom_id: "prev_page", label: "Previous", style: ButtonStyle.Success}),
+	new NextPageButton().setStyle({custom_id: "next_page", label: "Next", style: ButtonStyle.Success}),
+];
 
-	let embeds: Partial<Embed>[] = []
+async function gameEmbed(args: string[], channel: TextBasedChannel) {
+
+	let embeds: Embed | EmbedBuilder[] = []
 	let epicOffers = await getGames("GB", true)
 	let steamOffers = await getFreePromotions()
-	let s = new steam({
-		apiKey: process.env.STEAM,
-		format: 'json' //optional ['json', 'xml', 'vdf']
-	});
+	let s = new SteamApi.App(process.env.STEAM);
 
 	if(epicOffers.currentGames){
 		for(let game of epicOffers.currentGames){
 			let embed = new EmbedBuilder({
 				title: game.title,
 				description: game.description.substring(0, 1977),
-				fields: [
-					{
-						name: 'Store',
-						value: 'Epic Games'
-					}
-				],
-				thumbnail: {
-					url: game.keyImages.filter(i => i.type === 'Thumbnail')[0].url
+				footer: {
+					text: 'Epic Games',
 				},
-			})
-			embed.setColor(await getColorFromURL(game.keyImages.filter(i => i.type === 'Thumbnail')[0].url))
-			embeds.push(embed)
+				thumbnail: {
+					url: game.keyImages[0].url
+				},
+			});
+			embed.setColor(await getColorFromURL(game.keyImages.filter(i => i.type === 'Thumbnail')[0].url));
+			embeds.push(embed);
 		}
 	}
 
 	if(steamOffers.length > 0){
 		for(let game of steamOffers){
-			let id = game.link.split('https://store.steampowered.com/app/')[1]
-			console.log(id)
-			s.getStoreMetadata({
-				appid: id,
-				callback: function(err, data) {
-					console.log(data);
-				}
-			})
+			let id = game.link.split('https://store.steampowered.com/app/')[1];
+			let a = await s.appDetails(1281881);
 			let embed = new EmbedBuilder({
 				title: game.name,
+				description: a.description.substring(0, 1977),
 				url: game.link,
-				image: {
-					url: game.images[0]
+				footer: {
+					text: 'Steam',
 				},
-				fields: [
-					{
-						name: 'Store',
-						value: 'Steam'
-					}
-				],
 				thumbnail: {
 					url: game.header
 				},
-			})
-			embed.setColor(await getColorFromURL(game.header))
-			embeds.push(embed)
+			});
+			embed.setColor(await getColorFromURL(game.header));
+			embeds.push(embed);
 		}
 	}
 
-	return { embeds }
-
+	const pagination = new ChannelPagination().setButtons(buttons).setEmbeds(embeds).setTime(300000);
+	await pagination.send(channel);
 }
 
 export const games = new Command({
 	name: 'games',
-	alias: ['steam', 'epic'],
+	alias: ['epic', 'offers'],
 	desc: 'Check which games are free!',
 	category: 'Utility',
 	args: [],
@@ -80,9 +77,9 @@ export const games = new Command({
 	perms: ['SendMessages'], // https://discord-api-types.dev/api/discord-api-types-payloads/common#PermissionFlagsBits
 	bPerms: ['SendMessages'],
 	execute: async (message: Message, client: CustomClient, args: string[]) => {
-		return gameEmbed(args);
+		return gameEmbed(args, message.channel);
 	},
 	slashExecute: async (interaction: CommandInteraction, client: CustomClient, args: string[]) => {
-		return gameEmbed(args);
+		return gameEmbed(args, interaction.channel);
 	},
 })
